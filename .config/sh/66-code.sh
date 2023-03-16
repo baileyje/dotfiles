36df1b9dd1c21a127164136c6ed44cceb7147822
Sean M. Duncan <bitbutcher@gmail.com>
2023-03-16 03:42:55 -0700

    fixed nvim them and issue where code.ext.inst failed outside home directory

--- .config/sh/66-code.sh
+++ .config/sh/66-code.sh
@@ -5,7 +5,7 @@ if which codium > /dev/null; then code='codium'; fi
 dir="$HOME/.config/vscodium"
 alias code="$code --user-data-dir $dir --extensions-dir $dir/extensions"
 
-alias code.extensions.install='for ext in $(jq -r ".[] | .identifier.id" .config/vscodium/extensions/extensions.json); do code --install-extension $ext; done'
+alias code.extensions.install='for ext in $(jq -r ".[] | .identifier.id" ~/.config/vscodium/extensions/extensions.json); do code --install-extension $ext; done'
 alias code.ext.inst='code.extensions.install'
-alias code.extensions.update='for ext in $(jq -r ".[] | .identifier.id" .config/vscodium/extensions/extensions.json); do code --install-extension $ext --force; done'
+alias code.extensions.update='for ext in $(jq -r ".[] | .identifier.id" ~/.config/vscodium/extensions/extensions.json); do code --install-extension $ext --force; done'
 alias code.ext.up='code.extensions.update'
